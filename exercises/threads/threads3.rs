611eee447c668b8bbb0cdb80acab11492c454ff1
HarryPotterace <3276283138@qq.com>
2025-09-23 23:28:06 +0800

    update

--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -35,7 +35,7 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx.send(*val).unwrap();
+            tx1.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
